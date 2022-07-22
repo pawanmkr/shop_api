70d5a9eb274294f627e20c3a53351172b3aad6be
pawanmkr <iampawanmkr@gmail.com>
2022-07-22 13:49:00 +0530

    error handling done also added morgan package

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-import express from "express";
+import express, { application } from "express";
 import productRoutes from './api/routes/product.js';
 import orderRoutes from './api/routes/orders.js';
+import morgan from "morgan";
+
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('404 Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res , next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 export default app;

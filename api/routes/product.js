799ded6f03512b34a1ed53e06e4baea27f6f7f5e
pawanmkr <iampawanmkr@gmail.com>
2022-07-24 21:28:34 +0530

    a little more development into the project

--- api/routes/product.js
+++ api/routes/product.js
@@ -4,17 +4,35 @@ const Product = require('../models/products');
 
 const router = Express.Router();
 
+//GET Request for all entries
 router.get('/', (req, res) => {
-    res.send('product GET request');
+    Product.find().limit(100).
+    exec().
+    then((doc) => {
+        if (doc) {
+            console.log(doc);
+            res.status(200).json(doc);
+        } else {
+            res.send('Found 0 Products');
+        }
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json({error: err});
+    })
 });
 
+//GET ID Wise
 router.get('/:productId', (req, res) => {
     let id = req.params.productId;
     Product.findById(id).
     exec().
     then((doc) => {
-        console.log(doc);
-        res.status(200).json(doc);
+        if (doc) {
+            console.log(doc);
+            res.status(200).json(doc);
+        } else {
+            res.send('No valid entry found with this productID');
+        }
     }).
     catch((err) => {
         console.log(err);
@@ -22,6 +40,7 @@ router.get('/:productId', (req, res) => {
     });
 });
 
+//POST Request
 router.post('/', (req, res) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
@@ -40,12 +59,51 @@ router.post('/', (req, res) => {
     });
 });
 
+//PATCH for updating
 router.patch('/:productId', (req, res) => {
-    
+    const id = req.params.productId;
+    // const updateOperations = {};
+
+    // for (const ops of req.body) {
+    //     updateOperations[ops.propName] = ops.propValue;
+    // }
+    // Product.updateOne({_id: id}, { 
+    //     $set: updateOperations
+    // }).
+    // exec().
+    // then((result) => {
+    //     res.status(200).json(result);
+    // }).
+    // catch((err) => {
+    //     res.status(500);
+    //     res.send(err);
+    // });
+
+    Product.findByIdAndUpdate(id, {$set: req.body}, {new: true}).
+    exec().
+    then((result) => {
+        res.status(200).json(result);
+    }).
+    catch((err) => {
+        res.status(500);
+        res.send(err);
+    });
 });
 
+//DELETE for removing ID Wise
 router.delete('/:productId', (req, res) => {
-    res.send(`${req.params.productId} got deleted successfully!`);
+    let id = req.params.productId;
+    Product.remove({_id: id}).exec().then(() => {
+        res.send(`${req.params.productId} got deleted successfully!`);
+    }).catch(
+        (error) => {
+            res.status(500).json(
+                {
+                    error: error
+                }
+            );
+        }
+    );
 });
 
 module.exports = router;

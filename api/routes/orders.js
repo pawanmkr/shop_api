cb66fa43c3a1178ff9dec66c50909ccc6b8d87df
pawanmkr <iampawanmkr@gmail.com>
2022-07-25 14:03:40 +0530

    orders can be fetched by ID now and can be deleted also

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,10 +1,11 @@
 const Express = require('express');
 const mongoose = require('mongoose');
-const orders = require('../models/orders');
 const Orders = require('../models/orders');
+const Product = require('../models/products');
 
 const router = Express.Router();
 
+// fetch all the orders
 router.get('/', (req, res) => {
     Orders.find().select('product quantity _id')
     .exec()
@@ -35,31 +36,88 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-    const order = new Orders({
-        _id: mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        product: req.body.productId
-    });
-    order.save()
-    .then((result) => {
-        res.status(200);
-        res.send(result);
+// fetch single order
+router.get('/:orderId', (req, res) => {
+    let id = req.params.orderId;
+    Orders.findById(id).select('product quantity _id')
+    .exec()
+    .then((doc) => {
+        res.send({
+            Id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:9000/orders/' + doc._id
+            }
+        })
     })
     .catch((error) => {
-        res.send(error);
+        res.status(404);
+        res.send('Order do not exist');
+    });
+});
+
+// place/create order
+router.post('/', (req, res) => {
+    Product.findById(req.body.productId)
+    .then(product => {
+        const order = new Orders({
+            _id: mongoose.Types.ObjectId(),
+            quantity: req.body.quantity,
+            product: req.body.productId
+        })
+        return order.save();
+    })
+    .then((result) => {
+        res.status(200);
+        res.send({
+            Message: 'order stored successfully',
+            Details: {
+                ID: result._id,
+                product: result.product,
+                quantity: result.quantity
+            },
+            request: {
+                type: 'GET',
+                url: 'http://localhost:9000/orders/' + result._id
+            }
+        });
+    })
+    .catch((error) => {
+        res.status(500).send({
+            message: "This product is not available",
+            error: error
+        });
     });
 
 });
 
-router.patch('/:orderId', (req, res) => {
-    res.send(`address updated successfully...here's your order ID: 
-    ${req.params.orderId}`);
-});
-
-router.delete('/:orderId', (req, res) => {
-    res.send(`cancellation request accepted...
-    fuck you & money will be refunded...fuck you asshole again`);
+// delete orders
+router.delete('/:orderId', (req, res, next) => {
+    Orders.remove({ _id: req.params.orderId })
+    .exec()
+    .then((result) => {
+        res.status(200);
+        res.send({
+            Message: 'order deleted successfully',
+            Details: {
+                ID: result._id,
+                product: result.product,
+                quantity: result.quantity
+            },
+            request: {
+                type: 'POST',
+                url: 'http://localhost:9000/orders/' + req.params.orderId
+            }
+        });
+    })
+    .catch((error) => {
+        res.status(500).send({
+            message: "Can't find the given order or does not exist",
+            error: error
+        });
+    });
 });
 
 module.exports = router;
